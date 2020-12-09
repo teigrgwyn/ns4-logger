7005c67a89a6aa23dce335de027eac9d3254a59c
Cory Young <teigrgwyn@gmail.com>
2020-12-08 19:43:09 -0600

    set objectives/outline

---
+++ cr.py
@@ -0,0 +1,6 @@
+# Objectives:
+#   · Identify CR print
+#   · Save CR temporarily
+#   · Identify NPC death print
+#   · If we're expecting death print, save NPC into death array so we only print once at the very end
+#   · Print array to output alphabetically
